d7e7e8338df10bb47aa3f4ef0f1375512b14624d
Kyle Josling <kylejosling@hotmail.com>
2018-12-07 12:45:57 -0500

    Renamed and changed some signal names for binary to gray conversions

--- binary_gray_conversions/src/binary_gray_conversions.sv
+++ binary_gray_conversions/src/binary_gray_conversions.sv
@@ -1,5 +1,9 @@
-module bin2gray # (
-   parameter WIDTH = 4
+`timescale 1ns/1ps
+
+module gray2bin # (
+   
+   parameter WIDTH = 4 // Data width
+
 ) (
    
    input  logic clk_i,
@@ -10,17 +14,17 @@ module bin2gray # (
 
 );
 
-logic [WIDTH-1:0] in_data_dly;
-logic [WIDTH-1:0] out_data_dly;
+logic [WIDTH-1:0] in_data;
+logic [WIDTH-1:0] out_data;
 
 // Flop data to break timing
 always_ff @(posedge clk_i) begin
    if (rst_i) begin
-      in_data_dly <= 0;
+      in_data <= 0;
       out_data_o  <= 0;
    end else begin
-      in_data_dly <= in_data_i;
-      out_data_o <= out_data_dly;
+      in_data <= in_data_i;
+      out_data_o <= out_data;
    end
 end
 
@@ -31,11 +35,11 @@ generate
       // MSB is passed through
       if (i == WIDTH-1) begin
          always_comb begin
-            out_data_dly[WIDTH-1] = in_data_dly[WIDTH-1];     
+            out_data[WIDTH-1] = in_data[WIDTH-1];     
          end
       end else begin
          always_comb begin
-            out_data_dly[i] = in_data_dly[i+1] ^ in_data_dly[i];
+            out_data[i] = in_data[i+1] ^ in_data[i];
          end
       end
 
@@ -46,8 +50,8 @@ endgenerate
 
 endmodule
 
-module gray2bin # (
-   parameter WIDTH = 64
+module gray2bina # (
+   parameter WIDTH = 4 // Data width
 ) (
    
    input  logic clk_i,
